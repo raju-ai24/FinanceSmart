b266a6b355feb3f875f451f60b9eb5c069fe0323
rx-dev <rxpineple@gmail.com>
2026-04-18 23:10:15 +0530

    commmit by rj

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://financesmart-backend.onrender.com";
 
 //utils/apiPaths.js
 export const API_PATHS = {
